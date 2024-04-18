77c787ec00b063837e81e01f954b52c08d143f43
Tomi <tomasagustin.200358@gmail.com>
2024-04-18 15:02:32 -0300

    comenzando galeria

---
+++ practicas/05_galeria/views/index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Galería</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Galería con PHP y MySQL</h1>
+        </div>
+    </header>
+
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/sara-cervera-ORltc33fQXw-unsplash.jpg" alt="">
+                </a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
